b7cf315723eca408e276185a0aea42588e8be3a9
Stephen Hannon <stephenhannon4@gmail.com>
2020-12-15 12:08:56 -0600

    Day 15

---
+++ day15/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+const NumTurns = 30000000
+
+func main() {
+	ages := make(map[int]int)
+	var input []int
+	input = []int{0, 3, 6}
+	input = []int{3, 1, 2}
+	input = []int{0,14,6,20,1,4}
+
+	turn := 1
+	for ; turn <= len(input); turn++ {
+		num := input[turn - 1]
+		ages[num] = turn
+	}
+
+	prevNum := input[len(input) - 1]
+	for ; turn <= NumTurns; turn++ {
+		lastSeen, wasSeen := ages[prevNum]
+		var newNumber int
+		if wasSeen {
+			newNumber = turn - 1 - lastSeen
+		} else {
+			newNumber = 0
+		}
+		if (turn & 0xfffff) == 0 || turn == NumTurns {
+			fmt.Printf("%d.\t%d -> %d\n", turn, prevNum, newNumber)
+		}
+		ages[prevNum] = turn - 1
+		prevNum = newNumber
+	}
+}
+
